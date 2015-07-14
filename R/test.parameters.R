aac413e9887e4ec0a317fcda8af6d22f1e00dc33
coffeemuggler <mdietze@gfz-potsdam.de>
2015-07-14 14:53:52 +0200

    - mutlicore option implemented, thanks to Christoph Burow - progressbar option implemented

--- R/test.parameters.R
+++ R/test.parameters.R
@@ -30,6 +30,10 @@
 #' @param legend Character scalar, specifying legend position (cf.
 #' \code{\link{legend}}).  If omitted, no legend will be plotted, default is no
 #' legend.
+#' @param progressbar Logical scalar, optionally show a progress bar, default
+#' is \code{FALSE}. Only available if option \code{multicore} is not used.
+#' @param multicore Logical scalar, optionally ditribute calculations to all 
+#' available cores of the computer, default is \code{TRUE}.
 #' @param \dots Additional arguments passed to the plot function. Since the
 #' function returns two plots (except for plot option "ol"), additional
 #' graphical parameters must be specified as vector with the first element for
@@ -75,21 +79,22 @@
 #' 
 #' @export test.parameters
 test.parameters <-function(
-X,
-q, 
-lw = 0,
-c = 100,
-rotation = "Varimax",
-plot = FALSE,
-legend,
-progressbar = TRUE,
-...,
-pm = FALSE
+  X,
+  q, 
+  lw = 0,
+  c = 100,
+  rotation = "Varimax",
+  plot = FALSE,
+  legend,
+  progressbar = FALSE,
+  multicore = TRUE,
+  ...,
+  pm = FALSE
 ){
   ## definition of target vectors and matrices
   q.t <- rep(q, length(lw))
   lw.t <- rep(seq(min(lw), max(lw), length.out = length(lw)), 
-            each = q[length(q)] - 1)
+              each = q[length(q)] - 1)
   mEm <- rep(NA, length(q.t))
   mEn <- mEm
   mRm <- mEm
@@ -98,55 +103,99 @@ pm = FALSE
   ol  <- mEm
   i.pb <- 1
   
-  ## setup initial progress bar
-  if(progressbar == TRUE) {
+  ## optionally setup initial progress bar
+  if(progressbar == TRUE & multicore == FALSE) {
     pb <- txtProgressBar(min = 0, max = length(lw.t), char = "=", style = 3)
   }
   
-  ## loop through all test q-vector elements
-  for(i in 1:length(q.t)) {
-    ## perform EMMA with respective parameter combinations
-    EM <- try(EMMA(X, 
-                   q = q.t[i], 
-                   lw = lw.t[i], 
-                   c = c,
-                   rotation = rotation), silent = TRUE)
+  if(multicore == TRUE) {
     
-    ## test if EMMA resulted an error
-    test.EM <- as.logical(grepl("Error", EM[1]))
-    if (test.EM == TRUE) {
-      ## if EMMA was erroneous, set result values to NA
-      mEm[i] <- NA
-      mEn[i] <- NA
-      mRm[i] <- NA
-      mRn[i] <- NA
-      mRt[i] <- NA
-      ol[i]  <- NA
+    ## detect cores
+    cores <- parallel::detectCores()
+    
+    ## initiate cluster
+    cl <- parallel::makeCluster(getOption("mc.cores", cores))
+    
+    ## run EMMA in parallel
+    EM <- parallel::clusterMap(cl, function(qi, lwi, X, c, rotation) {
+      tryCatch(expr = {
+        library(EMMAgeo)
+        tmp <- EMMA(X = X, 
+                    q = qi, 
+                    lw = lwi, 
+                    c = c, 
+                    rotation = rotation)
+        c(mean(tmp$Em), 
+          mean(tmp$En), 
+          mean(tmp$Rm), 
+          mean(tmp$Rn), 
+          mean(c(tmp$Rm, tmp$Rn)), 
+          tmp$ol)
+      },
+      error = function(e) { NA })
+    },
+    q.t, 
+    lw.t, 
+    SIMPLIFY = TRUE, 
+    MoreArgs = list(X = X, 
+                    c = c, 
+                    rotation = rotation))
+    
+    ## stop cluster
+    parallel::stopCluster(cl) 
+    
+    # assign putput to target variables
+    mEm <- EM[1, ]
+    mEn <- EM[2, ]
+    mRm <- EM[3, ]
+    mRn <- EM[4, ]
+    mRt <- EM[5, ]
+    ol <- EM[6, ]
+  } else {
+    ## loop through all test q-vector elements
+    for(i in 1:length(q.t)) {
+      ## perform EMMA with respective parameter combinations
+      EM <- try(EMMA(X, 
+                     q = q.t[i], 
+                     lw = lw.t[i], 
+                     c = c,
+                     rotation = rotation), silent = TRUE)
       
-      ## update progress bar
-      if(progressbar == TRUE) {
-        setTxtProgressBar(pb, i)
+      ## test if EMMA resulted an error
+      test.EM <- as.logical(grepl("Error", EM[1]))
+      if (test.EM == TRUE) {
+        ## if EMMA was erroneous, set result values to NA
+        mEm[i] <- NA
+        mEn[i] <- NA
+        mRm[i] <- NA
+        mRn[i] <- NA
+        mRt[i] <- NA
+        ol[i]  <- NA
+        
+        ## update progress bar
+        if(progressbar == TRUE) {
+          setTxtProgressBar(pb, i)
+        }
+        
+      } else {
+        ## if EMMA was successful, assign result values
+        mEm[i] <- mean(EM$Em) # absolute row-wise model error vector
+        mEn[i] <- mean(EM$En) # absolute column-wise model error vector
+        mRm[i] <- mean(EM$Rm) # mean row-wise explained variance vector
+        mRn[i] <- mean(EM$Rn) # mean row-wise explained variance vector
+        mRt[i] <- mean(c(EM$Rm, EM$Rn)) # mean total explained variance vector
+        ol[i]  <- EM$ol # mean total explained variance vector is NA
+        
+        if(progressbar == TRUE & multicore == FALSE) {
+          setTxtProgressBar(pb, i) # update progress bar
+        }
       }
-      
-    } else {
-      ## if EMMA was successful, assign result values
-      mEm[i] <- mean(EM$Em) # absolute row-wise model error vector
-      mEn[i] <- mean(EM$En) # absolute column-wise model error vector
-      mRm[i] <- mean(EM$Rm) # mean row-wise explained variance vector
-      mRn[i] <- mean(EM$Rn) # mean row-wise explained variance vector
-      mRt[i] <- mean(c(EM$Rm, EM$Rn)) # mean total explained variance vector
-      ol[i]  <- EM$ol # mean total explained variance vector is NA
-      
-      if(progressbar == TRUE) {
-        setTxtProgressBar(pb, i) # update progress bar
-      }
-      
     }
-  }
-  
-  ## close progress bar
-  if(progressbar == TRUE) {
-    close(pb)
+    
+    ## close progress bar
+    if(progressbar == TRUE & multicore == FALSE) {
+      close(pb)
+    }
   }
   
   ## convert vectors to matrices
@@ -175,13 +224,13 @@ pm = FALSE
   ## read plot-independent plot parameters and check/set default values
   extraArgs <- list(...)
   colour <- if("colour" %in% names(extraArgs)) {extraArgs$colour} else
-    {seq(1, length(lw))}
+  {seq(1, length(lw))}
   legend.cex <- if("cex" %in% names(extraArgs)) {extraArgs$cex} else
-    {1}
+  {1}
   legend.lty <- if("lty" %in% names(extraArgs)) {extraArgs$lty} else
-    {1}
+  {1}
   legend.text <- if("legend" %in% names(extraArgs)) {extraArgs$legend} else
-    {round(lw, 3)}
+  {round(lw, 3)}
   legend.title <- if("title" %in% names(extraArgs)) {extraArgs$title} else
   {"lw"}
   
@@ -190,35 +239,35 @@ pm = FALSE
     ## read additional arguments list and check/set default values
     extraArgs <- list(...)
     main <- if("main" %in% names(extraArgs)) {extraArgs$main} else
-      {c(expression(paste("Model error (", bar(E)[m], ")", sep = "")), 
-         expression(paste("Model error (", bar(E)[m], ")", sep = "")))}
+    {c(expression(paste("Model error (", bar(E)[m], ")", sep = "")), 
+       expression(paste("Model error (", bar(E)[m], ")", sep = "")))}
     xlab <- if("xlab" %in% names(extraArgs)) {extraArgs$xlab} else
-      {c("Number of end-members (q)", 
-         "Number of end-members (q)")}
+    {c("Number of end-members (q)", 
+       "Number of end-members (q)")}
     ylab <- if("ylab" %in% names(extraArgs)) {extraArgs$ylab} else
-      {c(expression(paste("Weight limit (", l[w], ")", sep = "")),
-         expression(paste("Error (", bar(E)[m], ")", sep = "")))}
-        
+    {c(expression(paste("Weight limit (", l[w], ")", sep = "")),
+       expression(paste("Error (", bar(E)[m], ")", sep = "")))}
+    
     ## setup plot area
     par(mfcol = c(1, 2))
-
+    
     ## plot matrix q-lw as image
     image(q, lw, mEm,
-      col = rev(rainbow(n = 250, s = 1, v = 1, start = 0, end = 0.35)),
-      main = main[1],
-      xlab = xlab[1], 
-      ylab = ylab[1],
-      zlim = c(min(mEm, na.rm = TRUE), max(mEm, na.rm = TRUE)))
+          col = rev(rainbow(n = 250, s = 1, v = 1, start = 0, end = 0.35)),
+          main = main[1],
+          xlab = xlab[1], 
+          ylab = ylab[1],
+          zlim = c(min(mEm, na.rm = TRUE), max(mEm, na.rm = TRUE)))
     if(length(lw) > 1) contour(q, lw, mEm, add = TRUE,
-      zlim = c(min(mEm, na.rm = TRUE), max(mEm, na.rm = TRUE)))
+                               zlim = c(min(mEm, na.rm = TRUE), max(mEm, na.rm = TRUE)))
     
     ## plot mEm vs. q
     plot(seq(min(q), max(q)), mEm[,1], type = "l", 
-      main = main[2],
-      xlab = xlab[2],
-      ylab = ylab[2],
-      ylim = range(mEm, na.rm = TRUE),
-      col = colour[1])
+         main = main[2],
+         xlab = xlab[2],
+         ylab = ylab[2],
+         ylim = range(mEm, na.rm = TRUE),
+         col = colour[1])
     if(nrow(mEm > 1)) for (i in 2:length(lw)) {lines(mEm[,i], col = colour[i])}
     
     ## optionally add legend
@@ -261,7 +310,7 @@ pm = FALSE
           ylab = ylab[1],
           zlim = c(min(mEn, na.rm = TRUE), max(mEn, na.rm = TRUE)))
     if(length(lw) > 1) contour(q, lw, mEn, add = TRUE,
-      zlim = c(min(mEn, na.rm = TRUE), max(mEn, na.rm = TRUE)))
+                               zlim = c(min(mEn, na.rm = TRUE), max(mEn, na.rm = TRUE)))
     
     ## plot mEn vs. q
     plot(seq(min(q), max(q)), mEn[,1], type = "l", 
@@ -282,7 +331,7 @@ pm = FALSE
              lty = legend.lty,
              title = legend.title)
     }
-
+    
     ## reset plot area
     par(mfcol = c(1, 1))
   }
@@ -312,7 +361,7 @@ pm = FALSE
           ylab = ylab[1],
           zlim = c(min(mRm, na.rm = TRUE), max(mRm, na.rm = TRUE)))
     if(length(lw) > 1) contour(q, lw, mRm, add = TRUE,
-      zlim = c(min(mRm, na.rm = TRUE), max(mRm, na.rm = TRUE)))
+                               zlim = c(min(mRm, na.rm = TRUE), max(mRm, na.rm = TRUE)))
     
     ## plot mRm vs. q
     plot(seq(min(q), max(q)), mRm[,1], type = "l", 
@@ -337,7 +386,7 @@ pm = FALSE
     ## reset plot area
     par(mfcol = c(1,1))
   }
-    
+  
   ## optional plot mRn
   if(plot == "mRn") {
     ## read additional arguments list and check/set default values
@@ -363,7 +412,7 @@ pm = FALSE
           ylab = ylab[1],
           zlim = c(min(mRn, na.rm = TRUE), max(mRn, na.rm = TRUE)))
     if(length(lw) > 1) contour(q, lw, mRn, add = TRUE,
-      zlim = c(min(mRn, na.rm = TRUE), max(mRn, na.rm = TRUE)))
+                               zlim = c(min(mRn, na.rm = TRUE), max(mRn, na.rm = TRUE)))
     
     ## plot mRn vs. q
     plot(seq(min(q), max(q)), mRn[,1], type = "l", 
@@ -384,7 +433,7 @@ pm = FALSE
              lty = legend.lty,
              title = legend.title)
     }
-
+    
     ## reset plot area
     par(mfcol = c(1,1))
   }
@@ -414,7 +463,7 @@ pm = FALSE
           ylab = ylab[1],
           zlim = c(min(mRt, na.rm = TRUE), max(mRt, na.rm = TRUE)))
     if(length(lw) > 1) contour(q, lw, mRt, add = TRUE,
-      zlim = c(min(mRt, na.rm = TRUE), max(mRt, na.rm = TRUE)))
+                               zlim = c(min(mRt, na.rm = TRUE), max(mRt, na.rm = TRUE)))
     
     ## plot mRt vs. q
     plot(seq(min(q), max(q)), mRt[,1], type = "l", 
@@ -445,11 +494,11 @@ pm = FALSE
     ## read additional arguments list and check/set default values
     extraArgs <- list(...)
     main <- if("main" %in% names(extraArgs)) {extraArgs$main} else
-      {"Overlapping end-member loadings"}
+    {"Overlapping end-member loadings"}
     xlab <- if("xlab" %in% names(extraArgs)) {extraArgs$xlab} else
-      {"Number of end-members (q)"}
+    {"Number of end-members (q)"}
     ylab <- if("ylab" %in% names(extraArgs)) {extraArgs$ylab} else
-      {expression(paste("Weight limit (", l[w], ")", sep = ""))}
+    {expression(paste("Weight limit (", l[w], ")", sep = ""))}
     cex <- if("cex" %in% names(extraArgs)) {extraArgs$cex} else
     {0.6}
     
