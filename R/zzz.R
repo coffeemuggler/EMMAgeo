8d123903724a6df01afb0d232f93351f52e4b641
coffeemuggler <mdietze@gfz-potsdam.de>
2019-05-22 17:30:37 +0200

    CRAN submission preparation

--- R/zzz.R
+++ R/zzz.R
@@ -11,11 +11,16 @@
 .onAttach <- function(libname, pkgname){
   
   ## show startup message
-  try(packageStartupMessage(paste0(
-    "EMMAgeo v. 0.9.6. Please cite this package as: ",
-    "Dietze, E., and Dietze, M.: Grain-size distribution unmixing using ",
-    "the R package EMMAgeo, E&G Quaternary Sci. J., 69, 1-18, ",
-    "https://doi.org/10.5194/egqsj-69-1-2019, 2019. (Don't forget to ",
-    "mention the package and R version.)")),
+  try(packageStartupMessage(paste0("\n",
+    "EMMAgeo v. 0.9.6. When using this package please cite: ", "\n", "\n",
+    "Dietze, E., and Dietze, M.: Grain-size distribution unmixing using the ",
+    "R package EMMAgeo, E&G Quaternary Sci. J., 68, 29-46, https://doi.org/",
+    "10.5194/egqsj-68-29-2019, 2019", "\n", "\n",
+    "Dietze E, Hartmann K, Diekmann B, IJmker J, Lehmkuhl F, Opitz S, Stauch ",
+    "G, Wuennemann B, Borchers A. 2012. An end-member algorithm for ",
+    "deciphering modern detrital processes from lake sediments of ",
+    "Lake Donggi Cona, NE Tibetan Plateau, China. Sedimentary Geology ",
+    "243-244: 169-180.", "\n", "\n",
+    "(Don't forget to mention the package and R version.)")),
       silent = TRUE)
 }
