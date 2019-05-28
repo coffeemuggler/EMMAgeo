3722f68c36acd316074b66e7c8392127eb38e5b7
coffeemuggler <mdietze@gfz-potsdam.de>
2019-05-28 20:48:35 +0200

    version number updated to current branch

--- R/zzz.R
+++ R/zzz.R
@@ -12,7 +12,7 @@
   
   ## show startup message
   try(packageStartupMessage(paste0("\n",
-    "EMMAgeo v. 0.9.6. When using this package please cite: ", "\n", "\n",
+    "EMMAgeo v. 0.9.7. When using this package please cite: ", "\n", "\n",
     "Dietze, E., and Dietze, M.: Grain-size distribution unmixing using the ",
     "R package EMMAgeo, E&G Quaternary Sci. J., 68, 29-46, https://doi.org/",
     "10.5194/egqsj-68-29-2019, 2019", "\n", "\n",
